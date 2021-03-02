931023d1c674c5499751ee01e06e58299cf55c2f
Stusigh <stuartjohnpage@live.com>
2021-03-02 09:06:33 -0600

    removed fragment link, was not working

--- src/main/java/com/mywork/demoTwitterTheThird/service/TweetService.java
+++ src/main/java/com/mywork/demoTwitterTheThird/service/TweetService.java
@@ -1,4 +1,35 @@
 package com.mywork.demoTwitterTheThird.service;
 
+import java.util.List;
+
+import com.mywork.demoTwitterTheThird.model.Tweet;
+import com.mywork.demoTwitterTheThird.model.User;
+import com.mywork.demoTwitterTheThird.repository.TweetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TweetService {
+
+    @Autowired
+    private TweetRepository tweetRepository;
+
+    public List<Tweet> findAll() {
+        List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
+        return tweets;
+    }
+
+    public List<Tweet> findAllByUser(User user) {
+        List<Tweet> tweets = tweetRepository.findAllByUserOrderByCreatedAtDesc(user);
+        return tweets;
+    }
+
+    public List<Tweet> findAllByUsers(List<User> users){
+        List<Tweet> tweets = tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
+        return tweets;
+    }
+
+    public void save(Tweet tweet) {
+        tweetRepository.save(tweet);
+    }
 }
